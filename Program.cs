95f2d9f54e1c46bc5eeb3d75f9a4226a4a736179
unknown <you@example.com>
2021-03-02 23:12:06 +0900

    firstVersion

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 답정너_거울아_거울아
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            do
+            {
+                Console.WriteLine("하연이는 예쁜가요? (y/n) : ");
+                answer = Console.ReadLine();
+
+            } while ( answer != "y" );
+
+            Console.WriteLine("정답입니다 !");
+
+        }
+    }
+}
+
+
+
+
+//
